608afafb6b22be43bdecd98ed81f236c9e19a75b
QABrowncoy <proclacy@gmail.com>
2026-03-18 20:40:12 -0500

    Add files via upload
    Add manual test cases (257 test cases)

--- main.py
+++
@@ -1,9 +0,0 @@
-from selenium.common import TimeoutException
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-
-from pages import BugHuntShop2Page
-import time
-import data
